c926fbfb4b02bde0efbc0bdaf0edde9e0aea6819
surajnuchhin <surajnuchhin@gmail.com>
2016-11-23 23:49:42 +0530

    Added on 2016-11-23

---
+++ zerodha_assignnment/scrapecc.py
@@ -0,0 +1,57 @@
+import sys  
+from BeautifulSoup import BeautifulSoup
+from PyQt4.QtGui import *  
+from PyQt4.QtCore import *  
+from PyQt4.QtWebKit import *  
+from lxml import html 
+import redis
+from time import sleep
+
+class Scraper(QWebPage):  
+	def __init__(self, url):  
+		self.app = QApplication(sys.argv)   # create a browser APP
+		QWebPage.__init__(self)             #Initialize web page
+		self.loadFinished.connect(self._loadFinished)   #execute _loadFinished after loading is done
+		self.mainFrame().load(QUrl(url))      # Load Url
+		self.app.exec_()                      # Execute App
+  
+	def _loadFinished(self, result):  
+		self.frame = self.mainFrame()       # Main frame is what contains return HTML 
+		self.app.quit()                     # quit App
+
+	def scrape_and_push(self) :
+			result = self.frame.toHtml()  #Convert to Html
+			formatted_result = str(result.toAscii())  # convert html to Ascii
+			soup = BeautifulSoup(formatted_result)     #Beautiful Soup to parse HTML file
+			table = soup.find(id="topGainers")        # find a table by name topGainers
+	
+			i=0
+			dict={}
+			for row in table.findAll('tr')[1:]:    # parse each element of table and create a dictionary
+				col = row.findAll('td')
+				symbol_link = col[0].a['href']
+				symbol = symbol_link.split("=")[1]
+				ltp = col[1].string
+				change_percent = col[2].string
+				traded_qty = col[3].string
+				traded_value = col[4].string
+				open = col[5].string
+				high = col[6].string
+				low = col[7].string
+				prev_close = col[8].string
+				latest_exdate = col[9].string
+				#print symbol_link, symbol, ltp, change_percent, traded_qty, traded_value, open, high, low, prev_close, latest_exdate
+				dict[i] = [str(symbol_link), str(symbol), str(ltp), str(change_percent), str(traded_qty), str(traded_value), str(open), str(high), str(low),str( prev_close), str(latest_exdate)]
+				i=i+1
+			print "This is ",dict
+
+			conn = redis.Redis('localhost')   #Open a connection with redis
+			conn.hmset("pythonDict", dict)    # Push data to redis
+
+if __name__ == "__main__" :
+	url = 'https://www.nseindia.com/live_market/dynaContent/live_analysis/top_gainers_losers.htm?cat=G' 
+	try :
+			sc = Scraper(url) #create an instance
+			sc.scrape_and_push()	 # scrape and push Url
+	except Exception as e :
+			print "Error : ------",e
---
+++ zerodha_assignnment/setup.py
@@ -0,0 +1,38 @@
+import cherrypy
+import redis
+from mako.template import Template
+import os
+
+
+class DisplayScrape(object):
+    @cherrypy.expose
+    def index(self):
+	template = Template(filename="views/display_scraped_data.html")  #Load template file
+	conn = redis.Redis('localhost') #connect to redis 
+	d = conn.hgetall("pythonDict") # Get dictionary pythonDict stored in redis
+	d = {int(k):v.replace("[","").replace("]","").split("','")[0].split("', '")  for k,v in d.items()}  # All keys and values of dictionary d will be in string format you need convert them to int and list
+	html = template.render(mydata=d)  #Render To template file
+	return html # Return HTML to be displayed
+if __name__ == '__main__':
+	cherrypy.server.socket_host = '0.0.0.0' # Tell cherryPy to run  in 0.0.0.0 to give remote access
+	current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep # gives current_directory
+#	print current_dir
+	config = { #Cconfiguration 
+		    'global': {
+		'environment': 'production',
+       	 'log.screen': True,
+	        'server.socket_host': '0.0.0.0',
+	        'server.socket_port': 8080,
+	        'engine.autoreload_on': True,
+	        'log.error_file': os.path.join(current_dir, 'errors.log'),
+	        'log.access_file': os.path.join(current_dir, 'access.log'),
+	    },
+	    '/':{
+	        'tools.staticdir.root' : current_dir,  # root of static directory
+	    },
+	    '/static':{
+	        'tools.staticdir.on' : True,
+	        'tools.staticdir.dir' : 'static', #name of static directory
+	    },
+		}
+	cherrypy.quickstart(DisplayScrape(), '/', config)
