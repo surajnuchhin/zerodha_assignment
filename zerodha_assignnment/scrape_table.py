ee29a1109a552fc6f06e34eb9addd47d9056ca67
surajnuchhin <surajnuchhin@gmail.com>
2016-11-25 09:09:01 +0530

    Update scrape_table.py

--- zerodha_assignnment/scrape_table.py
+++ zerodha_assignnment/scrape_table.py
@@ -23,30 +23,36 @@ class Scraper(QWebPage):
 			result = self.frame.toHtml()  #Convert to Html
 			formatted_result = str(result.toAscii())  # convert html to Ascii
 			soup = BeautifulSoup(formatted_result)     #Beautiful Soup to parse HTML file
-			table = soup.find(id="topGainers")        # find a table by name topGainers
+			table = soup.find(id="topGainers")   
+			#print table     # find a table by name topGainers
 	
 			i=0
 			dict={}
 			for row in table.findAll('tr')[1:]:    # parse each element of table and create a dictionary
 				col = row.findAll('td')
-				symbol_link = col[0].a['href']
-				symbol = symbol_link.split("=")[1]
-				ltp = col[1].string
-				change_percent = col[2].string
-				traded_qty = col[3].string
-				traded_value = col[4].string
-				open = col[5].string
-				high = col[6].string
-				low = col[7].string
-				prev_close = col[8].string
-				latest_exdate = col[9].string
-				#print symbol_link, symbol, ltp, change_percent, traded_qty, traded_value, open, high, low, prev_close, latest_exdate
-				dict[i] = [str(symbol_link), str(symbol), str(ltp), str(change_percent), str(traded_qty), str(traded_value), str(open), str(high), str(low),str( prev_close), str(latest_exdate)]
-				i=i+1
+				#print len(col)
+				if col[0].a != None :
+					symbol_link = col[0].a['href']
+					symbol = symbol_link.split("=")[1]
+					ltp = col[1].string
+					change_percent = col[2].string
+					traded_qty = col[3].string
+					traded_value = col[4].string
+					open = col[5].string
+					high = col[6].string
+					low = col[7].string
+					prev_close = col[8].string
+					latest_exdate = col[9].string
+					#print symbol_link, symbol, ltp, change_percent, traded_qty, traded_value, open, high, low, prev_close, latest_exdate
+					dict[i] = [str(symbol_link), str(symbol), str(ltp), str(change_percent), str(traded_qty), str(traded_value), str(open), str(high), str(low),str( prev_close), str(latest_exdate)]
+					i=i+1
 			print "This is ",dict
 
 			conn = redis.Redis('localhost')   #Open a connection with redis
-			conn.hmset("pythonDict", dict)    # Push data to redis
+			for key in conn.hgetall("pythonDict").keys() :
+				conn.hdel("pythonDict",key)
+			if len(dict) > 0 :
+				conn.hmset("pythonDict", dict)    # Push data to redis
 
 if __name__ == "__main__" :
 	url = 'https://www.nseindia.com/live_market/dynaContent/live_analysis/top_gainers_losers.htm?cat=G' 
